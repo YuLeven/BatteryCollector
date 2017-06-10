707014c5570e1d3936ce144afbc61a8c00b5b4ae
Yuri Levenhagen <yurileven@gmail.com>
2017-06-09 20:58:01 -0400

    Initial commit

---
+++ Source/BatteryCollector/Pickup.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Pickup.generated.h"
+
+UCLASS()
+class BATTERYCOLLECTOR_API APickup : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APickup();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	//Return the mesh for the pickup
+	FORCEINLINE class UStaticMeshComponent* GetMesh() const { return PickupMesh; }
+
+	//Return wether or not the pickup is active
+	UFUNCTION(BlueprintPure, Category = "Pickup")
+	bool IsActive();
+
+	//Allows other classes to safely change wether or not the pikcup is active
+	UFUNCTION(BlueprintCallable, Category = "Pickup")
+	void SetActive(bool NewPickupState);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	//True when the pikcup can be used, false when pickup is deactivated
+	bool bIsActive;
+
+private:
+	//Static mesh to represent the pickup in the level
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pickup", meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* PickupMesh;
+	
+};
